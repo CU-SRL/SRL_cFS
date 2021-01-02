4a68941bfefb466248218251ded586ca439da740
japopich <japopich@hotmail.com>
2021-01-02 14:41:12 -0700

    Preparing for RTEMS build, commit two

--- yonix_defs/targets.cmake
+++ yonix_defs/targets.cmake
@@ -103,10 +103,3 @@ SET(MISSION_CPUNAMES cpu1)
 SET(cpu1_PROCESSORID 1)
 SET(cpu1_APPLIST ci_lab to_lab sch_lab)
 SET(cpu1_FILELIST cfe_es_startup.scr)
-
-# CPU2 example.  This is not built by default anymore but 
-# serves as an example of how one would configure multiple cpus.
-SET(cpu2_PROCESSORID 2)
-SET(cpu2_APPLIST ci_lab to_lab sch_lab)
-SET(cpu2_FILELIST cfe_es_startup.scr)
-
