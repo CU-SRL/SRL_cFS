37cb2d49af40d4105bd61cb4af7b225f8be0b634
Zoe Roy <zoro2037@colorado.edu>
2022-02-11 16:07:16 -0700

    Added new temperature sensor app. Barometer still needs some work done, but all sensor apps should be ready to test... excluding lps25h which is still not able to find source file even though I am looking right at it. cFS being dumb, but perfect time to debug! Everything looking good, but still some tasks: change msgids for all new sensor apps to unused ones, go through code for neatness, get data packets saving to ram or other folder, test on Beaglebone then LAUNCH.

---
+++ apps/max7502/fsw/platform_inc/max7502_msgids.h
@@ -0,0 +1,42 @@
+/************************************************************************
+**
+**      GSC-18128-1, "Core Flight Executive Version 6.7"
+**
+**      Copyright (c) 2006-2019 United States Government as represented by
+**      the Administrator of the National Aeronautics and Space Administration.
+**      All Rights Reserved.
+**
+**      Licensed under the Apache License, Version 2.0 (the "License");
+**      you may not use this file except in compliance with the License.
+**      You may obtain a copy of the License at
+**
+**        http://www.apache.org/licenses/LICENSE-2.0
+**
+**      Unless required by applicable law or agreed to in writing, software
+**      distributed under the License is distributed on an "AS IS" BASIS,
+**      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+**      See the License for the specific language governing permissions and
+**      limitations under the License.
+**
+** File: max7502_msgids.h
+**
+** Purpose: 
+**  Define MAX7502 Message IDs
+**
+** Notes:
+**
+**
+*************************************************************************/
+#ifndef _max7502_msgids_h_
+#define _max7502_msgids_h_
+
+#define MAX7502_CMD_MID            	0x18DF
+#define MAX7502_SEND_HK_MID        	0x18E0
+#define MAX7502_HK_TLM_MID		        0x08DE
+#define MAX7502_DATA_TLM_MID		    0x08DF
+
+#endif /* _max7502_msgids_h_ */
+
+/************************/
+/*  End of File Comment */
+/************************/
